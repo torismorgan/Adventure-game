9ce89e2cb1d4eab9b35e55f3591edc4ad13dd7d9
wilsonn <wilsonn@mailhost.cs.uleth.ca>
2022-09-10 21:45:47 +0000

    Update placebo_class.cpp

--- src/placebo_class.cpp
+++ src/placebo_class.cpp
@@ -1,4 +1,5 @@
-// Minimal source file for a placebo class that does nothing but allows the pipeline to pass.
+// Minimal source file for a placebo class that does nothing but allows the
+// pipeline to pass.
 
 #include "placebo_class.h"
 
