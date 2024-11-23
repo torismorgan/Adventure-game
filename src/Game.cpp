276181e89f5252861cccb4bcb92d01063429bd45
Orisakite Morgan-Owiriwa <o.morganowiriwa@uleth.ca>
2024-11-22 23:13:44 -0700

    game class shell src code

---
+++ src/Game.cpp
@@ -0,0 +1,29 @@
+
+#include "game.hpp"
+#include <iostream>
+
+//Game Class
+Game:: Game(): isGameOver(){
+
+}
+
+Game:: ~Game(){
+
+}
+
+void Game::processCommand(const std::string& command){
+
+}
+
+
+bool Game::checkWinCondition(){
+
+}
+
+void Game::start(){
+
+}
+
+void Game::endGame(){}
+
+
