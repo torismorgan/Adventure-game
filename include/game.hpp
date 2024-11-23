311a196d2427f80a52833aee26ca0790d0d1a18d
Orisakite Morgan-Owiriwa <o.morganowiriwa@uleth.ca>
2024-11-22 22:35:14 -0700

    Added display

--- include/game.hpp
+++ include/game.hpp
@@ -5,10 +5,18 @@
 #include <map>
 
 class Game{
-    private:
+private:
     Player player; //the player object
     std::map<std::string, Room*> rooms; //All rooms in the game
     bool isGameOver; //tracks if the game has ended
+
+    void setupRooms(); //imitializes all the rooms
+    void processCommand(const std::string command); //handles player input
+    void displayWelcomeMessage();
+    void displayHelp();
+    
+public:
+
 }
 
 
