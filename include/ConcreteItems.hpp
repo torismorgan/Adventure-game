7050a5b267cb67f5a85657c1b25a3f1b1334b615
okec2720 <okec2720@mailhost.cs.uleth.ca>
2024-11-24 22:51:42 +0000

    Added some things to the Concreteitems.h class

--- include/ConcreteItems.hpp
+++ include/ConcreteItems.hpp
@@ -9,3 +9,28 @@ public:
     Flashlight();
     void use() override;
 };
+class Key : public Item {
+public:
+    Key();
+    void use() override;
+};
+
+class Amulet : public Item {
+public:
+    Amulet();
+    void use() override;
+};
+
+class Map : public Item {
+public:
+    Map();
+    void use() override;
+};
+
+class Book : public Item {
+public:
+    Book();
+    void use() override;
+};
+
+#endif // CONCRETEITEMS_H
