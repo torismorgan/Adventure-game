1d7ab08a7df800c058eb10928381175de8311a8a
Orisakite Morgan-Owiriwa <o.morganowiriwa@uleth.ca>
2024-11-25 21:55:47 -0700

    added gamedisplay

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,23 +1,27 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
-#include <string>
 #include <map>
 #include <memory>
+#include <string>
+#include "ConcreteItems.hpp"
 #include "Player.hpp"
-#include "Room.hpp"
+#include "GameDisplay.hpp" 
 
 class Game {
  private:
-    std::shared_ptr<Player> player;
-    std::map<std::string, std::shared_ptr<Room>> rooms;
-    bool isGameOver;
-    void setupGame();
-    void processCommand(const std::string& command);
+  std::shared_ptr<Player> player;
+  std::map<std::string, std::shared_ptr<Room>> rooms;
+  bool isGameOver;
+  void setupGame();  // Initializes rooms and items
+  void processCommand(const std::string& command);  // Handles player input
+  bool checkWinCondition();  // Checks if the player has won
 
  public:
-    Game();
-    void start();
+  Game();   // Constructor
+  ~Game();  // Destructor
+
+  void start();  // Starts the game loop
 };
 
 #endif
---
+++ include/GameDisplay.hpp
@@ -0,0 +1,17 @@
+#ifndef GAMEDISPLAY_HPP
+#define GAMEDISPLAY_HPP
+
+#include <string>
+#include "Room.hpp"
+
+class GameDisplay {
+public:
+void displayWelcomeMessage(); // Display welcome messages
+void displayGoodbyeMessage(); // Display goodbye messages
+void displayRoomDescription(Room* room); // Display current room description
+void displayError(const std::string& error); // Display error messages
+void displayHelp(); // Display game instructions
+void displayWinMessage(); // Display a winning message
+};
+
+#endif // GAMEDISPLAY_HPP
