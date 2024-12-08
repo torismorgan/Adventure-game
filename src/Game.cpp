99f4a7054ac6ca853a6d44fb49c7b5c23723de67
okec2720 <okec2720@mailhost.cs.uleth.ca>
2024-12-08 08:32:26 +0000

    still fixing errors

--- src/Game.cpp
+++ src/Game.cpp
@@ -29,7 +29,7 @@ void Game::setupGame() {
 
     // Add items
    auto torch = std::make_shared<Torch>();
-auto key = std::make_shared<key>();
+auto keyitem = std::make_shared<Key>();
 
 
     // Room configuration
@@ -38,7 +38,7 @@ auto key = std::make_shared<key>();
     library->setExit("down", basement);
 
     foyer->addItem(torch);
-    library->addItem(key);
+    library->addItem(keyitem);
 
     foyer->setPuzzle(foyerPuzzle);
     foyer->setNPC(whisperingVoice);
